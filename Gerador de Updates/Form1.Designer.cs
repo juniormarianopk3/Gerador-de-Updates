53638d2c9f2a92269fe702e4179c8402ab9006fb
Junior <juniormariano@live.com>
2018-12-21 21:09:04 -0200

    Projeto iniciado

---
+++ Gerador de Updates/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Gerador_de_Updates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFileDialog = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblProgresso = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnFileDialog
+            // 
+            this.btnFileDialog.Location = new System.Drawing.Point(12, 111);
+            this.btnFileDialog.Name = "btnFileDialog";
+            this.btnFileDialog.Size = new System.Drawing.Size(118, 23);
+            this.btnFileDialog.TabIndex = 0;
+            this.btnFileDialog.Text = "Abir Arquivos";
+            this.btnFileDialog.UseVisualStyleBackColor = true;
+            this.btnFileDialog.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lblProgresso
+            // 
+            this.lblProgresso.AutoSize = true;
+            this.lblProgresso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgresso.ForeColor = System.Drawing.Color.DimGray;
+            this.lblProgresso.Location = new System.Drawing.Point(8, 61);
+            this.lblProgresso.Name = "lblProgresso";
+            this.lblProgresso.Size = new System.Drawing.Size(60, 24);
+            this.lblProgresso.TabIndex = 2;
+            this.lblProgresso.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 88);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(462, 17);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 145);
+            this.Controls.Add(this.lblProgresso);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnFileDialog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gerador de Updates";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFileDialog;
+        public System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblProgresso;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Gerador de Updates/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using Ionic.Zip;
+
+namespace Gerador_de_Updates
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblProgresso.Text = "Escolha os arquivos para serem compactados";
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            string pastaSelecionada = folderBrowserDialog1.SelectedPath;
+
+            string pastaArquivoZip = pastaSelecionada + "\\Updates\\"; // Aqui vamos informar para qual pasta devemos enviar o arquivo, neste caso será uma subpasta da pasta atual (Este método pode ser feito de diversas formas, está é uma bem simples)
+
+            if (Directory.Exists(pastaArquivoZip))
+            {
+                MessageBox.Show("Pasta Updates antiga excluida e arquivo UpdateList.txt", "Informativo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Directory.Delete(pastaArquivoZip, true);
+                File.Delete(pastaSelecionada + "\\UpdateList.txt");
+            }
+
+            Directory.CreateDirectory(pastaArquivoZip);
+            DirectoryInfo dir = new DirectoryInfo(pastaSelecionada);
+
+            var subpastas = dir.GetDirectories("*", SearchOption.AllDirectories);
+
+
+
+
+            var Root = Directory.GetFiles(pastaSelecionada, "*.*", SearchOption.AllDirectories);
+            foreach (string arquivo in Root)
+            {
+                try
+                {
+
+                    var ShaOne = new SHA1CryptoServiceProvider();
+                    byte[] data = File.ReadAllBytes(arquivo);
+                    var DataCrip = ShaOne.ComputeHash(data);
+                    string hash = BitConverter.ToString(DataCrip).Replace("-", string.Empty).ToLower();
+
+                    using (StreamWriter writer = File.AppendText(pastaArquivoZip + "\\UpdateList.txt"))
+                    {
+                        writer.WriteLine(arquivo.Replace(string.Format(pastaSelecionada + "\\"), string.Empty) + "," + hash);
+                    }
+
+                    string nomeArquivo = arquivo.Replace(string.Format(pastaSelecionada + "\\"), string.Empty);
+
+                    using (ZipFile file = new ZipFile())
+                    {
+                        foreach (var item in subpastas)
+                        {
+                            Directory.CreateDirectory(pastaArquivoZip + item.FullName.Replace(string.Format(pastaSelecionada), string.Empty));
+                        }
+                        file.AddFile(arquivo, "");
+                        file.Save(pastaArquivoZip + nomeArquivo + ".zip");
+                        Directory.Delete(pastaArquivoZip + "Updates");
+                        lblProgresso.BeginInvoke((MethodInvoker)delegate () { lblProgresso.Text = "Compactando arquivos, Por favor Aguarde..."; });
+                        btnFileDialog.BeginInvoke((MethodInvoker)delegate () { btnFileDialog.Enabled = false; });
+                        progressBar1.BeginInvoke((MethodInvoker)delegate () { progressBar1.Style = ProgressBarStyle.Marquee; });
+                    }
+
+                }
+
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.ToString());
+                }
+
+
+            }
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.BeginInvoke((MethodInvoker)delegate () { progressBar1.Style = ProgressBarStyle.Blocks; });
+
+            if (MessageBox.Show("Arquivos criados com sucesso na Pasta ArquivoZip", "Tudo ok!", MessageBoxButtons.OK, MessageBoxIcon.Information).Equals(DialogResult.OK))
+            {
+                lblProgresso.Text = "Escolha os arquivos a serem compactados";
+                btnFileDialog.BeginInvoke((MethodInvoker)delegate () { btnFileDialog.Enabled = true; });
+
+            }
+        }
+    }
+}
+
